b3bfa3d26179f2af5f9167f8363d9e00e4b7e657
philres <philipp.rescheneder@univie.ac.at>
2016-01-27 15:33:05 +0100

    Function for counting SNP number from VarScan output file added

--- slamdunk/dunks/snps.py
+++ slamdunk/dunks/snps.py
@@ -4,6 +4,7 @@
 from __future__ import print_function
 import os
 import subprocess
+import csv
 
 projectPath = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 varScanPath = os.path.join(projectPath, "bin", "VarScan.v2.4.1.jar")
@@ -25,3 +26,14 @@ def SNPs(inputBAM, outputSNP, referenceFile, minVarFreq, minCov, log, printOnly=
         varscan.wait()
     
     fileSNP.close()    
+    
+def countSNPsInFile(inputFile):
+    snpCount = 0
+    tcSnpCount = 0
+    with open(inputFile, "r") as snpFile:
+            snpReader = csv.reader(snpFile, delimiter='\t')
+            for row in snpReader:
+                if(row[2].upper() == "T" and row[3].upper() == "C"):
+                    tcSnpCount = tcSnpCount + 1
+                snpCount = snpCount + 1
+    return snpCount, tcSnpCount
