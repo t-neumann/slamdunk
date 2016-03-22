247e158ff2fc69277caa87de2ef4c93c3f13c0f7
philres <philipp.rescheneder@univie.ac.at>
2016-03-22 13:35:51 +0100

    Added -l/--local option for map dunk

--- slamdunk/dunks/mapper.py
+++ slamdunk/dunks/mapper.py
@@ -45,10 +45,15 @@ def runSam2bam(inFile, outFile, log, index=True, sort=True, delinFile=False, onl
         runIndexBam(outFile, log, verbose=verbose, dry=dry)
 
 
-def Map(inputBAM, inputReference, outputSAM, log, threads=1, parameter="--no-progress --slam-seq 2 -e" , outputSuffix="_ngm_slamdunk", trim5p=0, printOnly=False, verbose=True, force=True):    
+def Map(inputBAM, inputReference, outputSAM, log, localMapping, threads=1, parameter="--no-progress --slam-seq 2" , outputSuffix="_ngm_slamdunk", trim5p=0, printOnly=False, verbose=True, force=False):    
     if(trim5p > 0):
         parameter = parameter + " -5 " + str(trim5p)
                     
+    if(localMapping is True):
+        parameter = parameter + " -l "
+    else:
+        parameter = parameter + " -e "
+        
     if(checkStep([inputReference, inputBAM], [outputSAM], force)):
         run(ngmPath + " -r " + inputReference + " -q " + inputBAM + " -t " + str(threads) + " " + parameter + " -o " + outputSAM, log, verbose=verbose, dry=printOnly)
     else:
