0ed44209446f8e8c4b20419acc3499547f978223
philres <philipp.rescheneder@univie.ac.at>
2016-10-03 11:04:14 +0200

    nextgenmap added as subtree in git repository

--- bin/build-ngm.sh
+++ bin/build-ngm.sh
@@ -1,10 +1,14 @@
-git clone https://github.com/Cibiv/NextGenMap.git
+#git clone https://github.com/Cibiv/NextGenMap.git
 
-cd NextGenMap
+cd ../nextgenmap
 mkdir build
 cd build
 cmake ..
 make -j4
 
-cd ../..
-ln -s NextGenMap/bin/ngm-*/ngm
+cd ../../bin
+ln -fs ../nextgenmap/bin/ngm-*/ngm ngm
+
+# Update
+# git fetch nextgenmap master
+# git subtree pull --prefix nextgenmap nextgenmap master --squash
