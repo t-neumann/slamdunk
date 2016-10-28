affb54b2f5aa15f189701876508269f747e5695f
philres <philipp.rescheneder@univie.ac.at>
2016-10-28 20:15:39 +0200

    set softlinks for rnaseq simulator

--- slamdunk/contrib/build-rnaseqreadsimulator.sh
+++ slamdunk/contrib/build-rnaseqreadsimulator.sh
@@ -1,3 +1,5 @@
 wget https://github.com/davidliwei/RNASeqReadSimulator/archive/master.tar.gz -O RNASeqReadSimulator.tar.gz
 tar xvzf RNASeqReadSimulator.tar.gz
 rm RNASeqReadSimulator.tar.gz
+
+ln RNASeqReadSimulator-master/src/*.py .
