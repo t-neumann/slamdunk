04584c13e25652abff8150059b11598963ea47b7
Tobias Neumann <tobias.neumann.at@gmail.com>
2017-03-23 11:27:18 +0100

    Copyrights added to src files

--- bin/_preamble.py
+++ bin/_preamble.py
@@ -1,13 +1,19 @@
-# Copyright (c) Twisted Matrix Laboratories.
-# See LICENSE for details.
-    
-# This makes sure that users don't have to set up their environment
-# specially in order to run these programs from bin/.
-    
-# This helper is shared by many different actual scripts.  It is not intended to
-# be packaged or installed, it is only a developer convenience.  By the time
-# Twisted is actually installed somewhere, the environment should already be set
-# up properly without the help of this tool.
+# Copyright (c) 2015 Tobias Neumann, Philipp Rescheneder.
+#
+# This file is part of Slamdunk.
+# 
+# Slamdunk is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# 
+# Slamdunk is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
     
 import sys, os
     
