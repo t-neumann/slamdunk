706201d2d24a84ec78bc59419d55880bc85c9ad7
philres <philipp.rescheneder@univie.ac.at>
2017-03-25 12:22:50 +0100

    travis

--- slamdunk/test/test_sample.sh
+++ slamdunk/test/test_sample.sh
@@ -1,5 +1,5 @@
 
-bin/slamdunk all -r slamdunk/test/data/ref.fa -b slamdunk/test/data/actb.bed -o slamdunk/test/data/output -rl 100 -mbq 27 -5 0 slamdunk/test/data/reads.fq
+slamdunk all -r slamdunk/test/data/ref.fa -b slamdunk/test/data/actb.bed -o slamdunk/test/data/output -rl 100 -mbq 27 -5 0 slamdunk/test/data/reads.fq
 
 diff slamdunk/test/data/reads_slamdunk_mapped_filtered_tcount.tsv slamdunk/test/data/output/count/reads_slamdunk_mapped_filtered_tcount.tsv 
 
