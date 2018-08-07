83e44b62118b935d7825d8b61d0025ea4101368a
tobias.neumann <tobias.neumann.at@gmail.com>
2018-08-07 12:01:01 +0200

    Nextgenmap Symlink -> Copy.

--- slamdunk/contrib/build-ngm.sh
+++ slamdunk/contrib/build-ngm.sh
@@ -26,4 +26,4 @@ make -j4
 cd ../..
 # Get version from version.py
 version=`grep "__ngm_version__" ../version.py | cut -d "\"" -f 2`
-ln -fs nextgenmap/bin/ngm-${version}/ngm ngm
+cp -r nextgenmap/bin/ngm-${version}/* .
