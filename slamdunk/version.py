fd1c8f5b4f6d3097c25d3fe8b7ec8d88d521a959
tobias.neumann <tobias.neumann.at@gmail.com>
2019-02-10 13:16:40 +0100

    Version 0.3.4

--- slamdunk/version.py
+++ slamdunk/version.py
@@ -16,7 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # Overall slamDunk version
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 # File format version of BAM files from slamdunk filter
 __bam_version__ = "3"
 # File format version of count files from slamdunk count
