6306fce7dfac971cc6f32cb74713b740928c5acd
tobias.neumann <tobias.neumann@imp.ac.at>
2019-12-09 22:04:57 +0100

    Removed nextgenmap subtree.

--- slamdunk/contrib/nextgenmap/src/Version.h
+++
@@ -1,9 +0,0 @@
-#ifndef VERSION_H
-#define VERSION_H
- 
-#define VERSION_MAJOR "0"
-#define VERSION_MINOR "5"
-#define VERSION_BUILD "2"
-
-#endif // VERSION_H
-
--- slamdunk/dunks/snps.py
+++ slamdunk/dunks/snps.py
@@ -3,17 +3,17 @@
 # Copyright (c) 2015 Tobias Neumann, Philipp Rescheneder.
 #
 # This file is part of Slamdunk.
-# 
+#
 # Slamdunk is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
-# 
+#
 # Slamdunk is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -25,24 +25,24 @@ from slamdunk.utils.misc import checkStep, getBinary  # @UnresolvedImport
 def SNPs(inputBAM, outputSNP, referenceFile, minVarFreq, minCov, minQual, log, printOnly=False, verbose=True, force=False):
     if(checkStep([inputBAM, referenceFile], [outputSNP], force)):
         fileSNP = open(outputSNP, 'w')
-        
-        mpileupCmd = getBinary("samtools") + " mpileup -B -A -f " + referenceFile + " " + inputBAM
+
+        mpileupCmd = "samtools mpileup -B -A -f " + referenceFile + " " + inputBAM
         if(verbose):
             print(mpileupCmd, file=log)
         if(not printOnly):
             mpileup = subprocess.Popen(mpileupCmd, shell=True, stdout=subprocess.PIPE, stderr=log)
-            
+
         varscanCmd = "java -jar " + getBinary("VarScan.v2.4.1.jar") + " mpileup2snp  --strand-filter 0 --output-vcf --min-var-freq " + str(minVarFreq) + " --min-coverage " + str(minCov) + " --variants 1"
         if(verbose):
             print(varscanCmd, file=log)
         if(not printOnly):
             varscan = subprocess.Popen(varscanCmd, shell=True, stdin=mpileup.stdout, stdout=fileSNP, stderr=log)
             varscan.wait()
-        
+
         fileSNP.close()
     else:
-        print("Skipping SNP calling", file=log)    
-        
+        print("Skipping SNP calling", file=log)
+
 def countSNPsInFile(inputFile):
     snpCount = 0
     tcSnpCount = 0
